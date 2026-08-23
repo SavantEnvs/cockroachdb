cafef82fe8483854ef00a52a0abd78c117ee7770
Saulius Menkevicius <saulius.menkevicius@bugcrowd.com>
2026-08-23 13:50:48 +0000

    Mayhem integration: cockroachdb 7 OSS-Fuzz targets (Option B — Bazel online codegen, offline go-build rebuild)

---
+++ mayhem/vendor-generated/pkg/geo/geoproj/zcgo_flags.go
@@ -0,0 +1,7 @@
+// GENERATED FILE DO NOT EDIT
+
+package geoproj
+
+// #cgo CPPFLAGS: -I/mayhem/bin/c-deps/archived_cdep_libjemalloc_linux/include -I/mayhem/bin/c-deps/archived_cdep_libkrb5_linux/include
+// #cgo LDFLAGS: -L/mayhem/bin/c-deps/archived_cdep_libjemalloc_linux/lib -L/mayhem/bin/c-deps/archived_cdep_libproj_linux/lib -L/mayhem/bin/c-deps/archived_cdep_libkrb5_linux/lib
+import "C"
